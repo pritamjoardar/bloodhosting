a262c573edc512035ef00b656484293c72664f11
Pritam Joardar <111422356+pritamjoardar@users.noreply.github.com>
2023-11-22 09:06:07 +0530

    solve path spelling

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const Connect =async()=>{
     });
 }     
 Connect();
-app.use(express.static(opath.join(__dirname,'client/build/')));
+app.use(express.static(path.join(__dirname,'client/build/')));
 if(process.env.NODE_ENV === 'production'){
     app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'client/build/index.html'))
